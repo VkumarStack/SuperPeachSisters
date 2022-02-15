671d886249e71e923e1b0798af1afe9ae66bfa01
VkumarStack <vksogi@gmail.com>
2022-02-15 14:54:40 -0800

    Started workign on doSomething() for Peach

--- SuperPeachSisters/Actor.cpp
+++ SuperPeachSisters/Actor.cpp
@@ -2,10 +2,10 @@
 #include "StudentWorld.h"
 
 // Students:  Add code to this file, Actor.h, StudentWorld.h, and StudentWorld.cpp
-Actor::Actor(int imageID, int startX, int startY, int startDirection, StudentWorld* world) : GraphObject(imageID, startX, startY)
+Actor::Actor(int imageID, int startX, int startY, int startDirection, StudentWorld* world, int dir = 0, int depth = 0, double size = 1.0) : GraphObject(imageID, startX, startY, dir, depth, size)
 {
 	m_studentWorld = world;
-	isAlive = true;
+	m_isAlive = true;
 }
 
 bool Actor::operator<(const Actor& other) const
@@ -13,9 +13,9 @@ bool Actor::operator<(const Actor& other) const
 	// Interactable (fireballs, goodies) Actors should have higher priority since their actions can change more behavior
 	// i.e. The action of a fireball hitting an enemy should be performed first since beyond so the enemy can no longer 
 	// attack Peach if interacting with her 
-	if (isInteractable() && !other.isInteractable())
+	if (usable() && !other.usable())
 		return true;
-	else if (other.isInteractable() && !isInteractable())
+	else if (other.usable() && !usable())
 		return false;
 
 	// All non-interactable Actors (meaning those that are NOT goodies, fire flowers, or peach) are intended to stay in a fixed vertical 
@@ -23,6 +23,77 @@ bool Actor::operator<(const Actor& other) const
 	// since vertical position will stay the same)
 	if (getY() < other.getY())
 		return true;
-	else
+	else if (getY() > other.getY())
 		return false; 
+}
+/*------------------------------------------------------------------------------------------------------------------------------*/
+Block::Block(int startX, int startY, StudentWorld* world, int goodie) : Actor(IID_BLOCK, startX, startY, 0, world, 2, 1)
+{
+	m_containsGoodie = goodie;
+}
+
+void Block::getBonked(const Actor& actor) 
+{
+	if (actor.player())
+	{
+		if (m_containsGoodie == 0)
+			getStudentWorld()->playSound(SOUND_PLAYER_BONK);
+		// TODO IMPLEMENT POWERUPS
+	}
+}
+
+
+
+/*------------------------------------------------------------------------------------------------------------------------------*/
+Peach::Peach(int startX, int startY, StudentWorld* world) : Actor(IID_PEACH, startX, startY, 0, world)
+{
+	m_hitPoints = 1;
+	m_jumpPower = false;
+	m_shootPowerTicks = -1;
+	m_starPowerTicks = 0;
+	m_tempInvincibilityTicks = 0;
+}	
+
+void Peach::doSomething()
+{
+	if (!alive())
+		return;
+	if (getStarPower())
+		m_starPowerTicks--;
+	if (getTempInvincibility())
+		m_tempInvincibilityTicks--;
+	if (m_shootPowerTicks > 0)
+		m_shootPowerTicks--;
+	
+	int key = -1;
+	if (getStudentWorld()->getKey(key))
+	{
+		switch (key)
+		{
+			case KEY_PRESS_LEFT:
+				setDirection(180);
+				Actor* actor = nullptr;
+				if (!isBlockingObjectAt(getX() - 4, getY()))
+					moveTo(getX() - 4, getY());
+				else if (actor != nullptr)
+				{
+					actor->getBonked(*this);
+				}
+				break;
+			case KEY_PRESS_RIGHT:
+				setDirection(0);
+				Actor* actor = nullptr;
+				if (!isBlockingObjectAt(getX() + 4, getY()))
+					moveTo(getX() + 4, getY());
+				else if (actor != nullptr)
+				{
+					actor->getBonked(*this);
+				}
+				break;
+			case KEY_PRESS_UP:
+				break;
+			case KEY_PRESS_SPACE:
+				break;
+		}
+	}
 }
--- SuperPeachSisters/Actor.h
+++ SuperPeachSisters/Actor.h
@@ -3,19 +3,67 @@
 
 #include "GraphObject.h"
 
+class StudentWorld;
+
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
 class Actor : public GraphObject
 {
 	public:
-		Actor(int imageID, int startX, int startY, int startDirection, StudentWorld* world);
+		Actor(int imageID, int startX, int startY, int startDirection, StudentWorld* world, int dir = 0, int depth = 0, double size = 1.0);
 		bool operator<(const Actor& other) const; // Define < operator for use in STL data structures 
-		void setDead() { isAlive = false; }
-		virtual bool canWalk() const { return true; }
-		virtual bool isInteractable() const { return false; }
-		virtual void bonk() const = 0;
+		
+		virtual void doSomething() = 0;
+		virtual void getBonked(const Actor& actor) = 0;
+		
+		void setDead() { m_isAlive = false; }
+
+		StudentWorld* getStudentWorld() const { return m_studentWorld; }
+		virtual bool stationary() const { return false; } // If the Actor has functionality not involving moving (i.e. Blocks are stationary but Koopas are not)
+		virtual bool friendly() const { return true; } // If the Actor has functionality that helps/does not harm the player (i.e. Blocks and Star Powers are friendly but Piranhas are not)
+		virtual bool sentient() const { return true; } // If the Actor has functionality that involves any sort of action (effectively everything except for the Block and Pipe are sentient)
+		virtual bool usable() const { return false; } // If the Actor has functionality that can be "used" by a player (i.e. Blocks, Flowers, Flags, etc. are usable)
+		virtual bool player() const { return false; } // If the Actor is Peach
+		virtual bool alive() const { return m_isAlive; }
 	private:
 		StudentWorld* m_studentWorld; // Access other actors contained in the StudentWorld object
-		bool isAlive;
+		bool m_isAlive;
+};
+
+/*------------------------------------------------------------------------------------------------------------------------------*/
+class Block : public Actor
+{
+	public:
+		Block(int startX, int startY, StudentWorld* world, int goodie);
+		virtual void doSomething() {};
+		virtual void getBonked(const Actor& actor);
+		virtual bool stationary() const { return true; }
+		virtual bool sentient() const { return false; }
+		virtual bool usable() const { return true; }
+	private:
+		int m_containsGoodie; // 0 = No Goodie, 1 = Mushroom, 2 = Flower, 3 = Star 
+};
+
+/*----------------------------------------------------------------------------------------------------------------------------*/
+
+class Peach : public Actor
+{
+public:
+	Peach(int startX, int startY, StudentWorld* world);
+	virtual void doSomething();
+	virtual void getBonked(const Actor& actor) {}
+
+	virtual bool player() const { return true; }
+	int getHitPoints() const { return m_hitPoints; }
+	bool getJumpPower() const { return m_jumpPower; }
+	bool getShootPower() const { return m_shootPowerTicks != -1; }
+	bool getStarPower() const { return m_starPowerTicks != 0; }
+	bool getTempInvincibility() const { return m_tempInvincibilityTicks != 0; }
+private:
+	int m_hitPoints;
+	bool m_jumpPower;
+	int m_shootPowerTicks;
+	int m_starPowerTicks;
+	int m_tempInvincibilityTicks;
 };
 
 #endif // ACTOR_H_
